dd92682f6007f4b85309b1bf5c5d5c0c9d04113e
Jakub Rakoczy <kuba.rakoczy@gmail.com>
2015-07-18 22:17:37 +0200

    Implement a basic timer

--- src/gittoggl.py
+++ src/gittoggl.py
@@ -1,6 +1,6 @@
 import os
 import signal
-from gi.repository import Gtk
+from gi.repository import Gtk, GLib
 from gi.repository import AppIndicator3 as AppIndicator
  
 APPINDICATOR_ID = 'myAppIndicator'
@@ -15,14 +15,21 @@ def app():
     
     Gtk.main()
 
+def update_timer_label(item_timer):
+    current_time = int(item_timer.get_label())
+    item_timer.set_label(str(current_time + 1))
+    return True
+
 def popup_menu(): 
     menu = Gtk.Menu() 
  
-    item_about = Gtk.MenuItem(label="00:00 Start") 
-    menu.append(item_about) 
+    item_timer = Gtk.MenuItem(label="0") 
+    menu.append(item_timer) 
+
+    GLib.timeout_add(1000, update_timer_label, item_timer)
     
-    item_quit = Gtk.MenuItem(label="Commit") 
-    menu.append(item_quit)
+    item_commit = Gtk.MenuItem(label="Commit") 
+    menu.append(item_commit)
      
     item_overview = Gtk.MenuItem(label="Overview") 
     menu.append(item_overview)
@@ -33,6 +40,8 @@ def popup_menu():
     menu.show_all() 
  
     return menu
+
+
  
 if __name__ == "__main__":
     signal.signal(signal.SIGINT, signal.SIG_DFL)
